f21710883a137cca1dcc7a03675c7dfb19c9e7bd
Thejaswin S <52855622+thejaswin123@users.noreply.github.com>
2023-03-02 14:44:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -150,6 +150,6 @@ app.post("/login",function(req,res){
     });
 });
 
-app.listen(3000,function(){
+app.listen(process.env.PORT||3000,function(){
     console.log("server starting at port 3000!");
 });
